f36961d188c6366547e3edb9977ca95a6b262315
Roja Mogili <roja.mogili@techvedika.com>
2024-11-30 16:39:02 +0530

    remove unused files

--- sales-portal/src/app/app-routing.module.ts
+++ sales-portal/src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)},
-  {path: 'books', loadChildren: () => import('./books/books.module').then(m => m.BooksModule)},
-  {path: 'fashion', loadChildren: () => import('./fashion/fashion.module').then(m => m.FashionModule)},
+  {path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)}
 ]
 
 @NgModule({
--- sales-portal/src/app/dashboard/dashboard.component.ts
+++ sales-portal/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
 
 import { HttpService } from './http.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [MessageService]
+
 })
 export class DashboardComponent implements OnInit {
 
@@ -16,7 +21,6 @@ export class DashboardComponent implements OnInit {
     {rating: 4, name: 'king of pride', price: 200, image: 'https://i0.wp.com/jeevesreads.com/wp-content/uploads/2023/03/img_9224.jpg?resize=736%2C1104&ssl=1', },
     {rating: 4, name: 'Blue Ridge', price: 500, image: 'https://images.pexels.com/photos/1667071/pexels-photo-1667071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
     {rating: 4, name: 'Blue Ridge', price: 50000, image: 'https://images.pexels.com/photos/279906/pexels-photo-279906.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'},
-    {rating: 4, name: 'Twisted Love', price: 200, image: 'https://rukminim2.flixcart.com/image/850/1000/xif0q/regionalbooks/h/u/v/twisted-love-original-imagqxwjjqzakaqw.jpeg?q=90&crop=false', },
   ]
 
   is_add_product: boolean = false;
@@ -25,26 +29,26 @@ export class DashboardComponent implements OnInit {
 
   constructor(
     private httpService: HttpService,
-    private cdr: ChangeDetectorRef
-
+    private cdr: ChangeDetectorRef,
+    private messageService: MessageService, 
+    private primengConfig: PrimeNGConfig
   ) { }
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
     this.fetchProducts();
   }
 
   fetchProducts() {
-    const urlPath = `http://localhost:5200/products/list` + (this.search_str ? `?name=${this.search_str}` : '');
+    const urlPath = `${environment.server_url}/products/list` + (this.search_str ? `?name=${this.search_str}` : '');
 
     this.httpService.getRequest( urlPath).subscribe((response: any) => {
-      console.log("--------res---------", response)
-      this.products = response.data
+      this.products = response.data;
     }, (error: any) => {
     });
   }
 
   search(value: any) {
-    console.log('-------val-------', value.target.value)
     this.search_str = value.target.value;
     if (this.search_str.length >= 3 || !this.search_str.length) {
       this.fetchProducts()
@@ -53,15 +57,12 @@ export class DashboardComponent implements OnInit {
 
   add_product(type: string) {
     this.is_add_product = type === 'add' ? true : false;
-    console.log("----", this.is_add_product)
     this.cdr.detectChanges()
-    
   }
 
   close_tab() {
     this.is_add_product = false;
     this.fetchProducts();
-
   }
 
 }
